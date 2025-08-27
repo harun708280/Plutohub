05fb08ab05cebce1da7e0908f66f398e68a38384
harun708280 <harun708280@gmail.com>
2025-08-27 17:42:51 +0600

    update

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,18 @@
 import BlogDetailsPage from "../../../Components/BlogDetailsPage/BlogDetailsPage";
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
 
-export default function Page({ params }: PageProps) {
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   return <BlogDetailsPage params={params} />;
+}
+
+
+export async function generateStaticParams() {
+  
+  const ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  return ids.map((id) => ({ id }));
 }
