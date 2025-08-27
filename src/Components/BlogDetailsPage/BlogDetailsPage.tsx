2d2a5b92a0b051e20f40a7dc320477f27bb8f18c
harun708280 <harun708280@gmail.com>
2025-08-27 17:49:23 +0600

    update

--- src/Components/BlogDetailsPage/BlogDetailsPage.tsx
+++ src/Components/BlogDetailsPage/BlogDetailsPage.tsx
@@ -5,25 +5,24 @@ import TableOfContents from "./TableOfContents/TableOfContents";
 import BlogContent from "./BlogContent/BlogContent";
 import RightSidebar from "./RightSidebar/RightSidebar";
 
+interface BlogDetailsPageProps {
+  params: {
+    id: string;
+  };
+}
 
-
-
-const BlogDetailsPage = () => {
+const BlogDetailsPage = ({ params }: BlogDetailsPageProps) => {
   return (
     <div className="container py-4 blog-details ">
+      
       <Tab.Container defaultActiveKey="stock">
         <div className="row">
-          {/* Left Sidebar */}
           <div className="col-lg-3 mb-4">
             <TableOfContents />
           </div>
-
-          {/* Middle Blog Content */}
           <div className="col-lg-6 mb-4 ">
             <BlogContent />
           </div>
-
-          {/* Right Sidebar */}
           <div className="col-lg-3">
             <RightSidebar />
           </div>
--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,18 @@
 import BlogDetailsPage from "../../../Components/BlogDetailsPage/BlogDetailsPage";
 
-
-export default async function Page({
+export default function Page({
   params,
 }: {
   params: { id: string };
 }) {
-  return <BlogDetailsPage  />;
+  return <BlogDetailsPage params={params} />;
 }
 
 
 export async function generateStaticParams() {
-  
   const ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  return ids.map((id) => ({ id }));
+  return ids.map((id) => ({
+    id,
+  }));
 }
